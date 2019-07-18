16200cbfc37a377ffab1ee5a4ce9b0e4080ed7a7
Pimin Konstantin Kefaloukos <pimin.konstantin.kefaloukos@maersk.com>
2019-07-18 14:27:36 +0200

    added counter

--- flaskdemo/server.py
+++ flaskdemo/server.py
@@ -1,10 +1,23 @@
 from flask import Flask, send_from_directory, render_template
-
+import redis
 import socket
 
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 hostname = socket.gethostname()
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.errorhandler(500)
@@ -14,8 +27,11 @@ def error(e):
 
 @app.route("/")
 def index():
-    return render_template( "index.html",
-        hostname=hostname)
+    try:
+        numhits = get_hit_count()
+    except:
+        numhits = '? (counter unavailable)'
+    return render_template("index.html", hostname=hostname, numhits=numhits)
 
 
 @app.route("/assets/<path:path>")
